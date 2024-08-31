279d23896544fd34dae6620c9b92f36c67d3aa76
Mirco Baalmans <mircobaalmans@live.nl>
2024-08-31 15:33:38 +0200

    Changed buttons to be made  in loop and made the random number one func

--- App.py
+++ App.py
@@ -1,22 +1,29 @@
 from guizero import App, Text, PushButton
-import math
+import math, random
 
-from DiceFunctions import *
+def rollDice(button,max):
+    button.text = random.randint(1,max)
 
-app = App(title="Dice",width=480,height=320,layout="grid",)
+app = App(title="Dice",width=480,height=320,layout="grid")
 
-columnWidth = math.ceil(480/3)
-rowHeight = math.ceil(320/2)
+cols = 3
+rows = 2
+diceTypes = [4,6,8,10,12,20]
+buttons = {}
 
-buttonD4 = PushButton(app,command=rollD4,image="Assets/Sprites/d4.png",grid=[0,1],width=columnWidth,height=rowHeight)
-buttonD6 = PushButton(app,command=rollD6,image="Assets/Sprites/d6.png",grid=[1,1],width=columnWidth,height=rowHeight)
-buttonD8 = PushButton(app,command=rollD8,image="Assets/Sprites/d8.png",grid=[2,1],width=columnWidth,height=rowHeight)
+columnWidth = math.ceil(480/cols)
+rowHeight = math.ceil(320/rows)
 
-buttonD10 = PushButton(app,command=rollD10,image="Assets/Sprites/d10.png",grid=[0,2],width=columnWidth,height=rowHeight)
-buttonD12 = PushButton(app,command=rollD12,image="Assets/Sprites/d12.png",grid=[1,2],width=columnWidth,height=rowHeight)
-buttonD20 = PushButton(app,command=rollD20,image="Assets/Sprites/d20.png",grid=[2,2],width=columnWidth,height=rowHeight)
+x,y = 0,0
+for i in diceTypes:
+    button = PushButton(app,image="Assets/Sprites/d"+ str(i) +".png",grid=[x,y],width=columnWidth,height=rowHeight)
+    button.update_command(rollDice,[button,i])
+    buttons[i] = button
+
+    x +=1
+    
+    if(x == cols): 
+        x = 0
+        y += 1
 
 app.display()
-
-
-
